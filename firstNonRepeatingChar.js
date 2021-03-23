ac8a12377f5ee87040b9de707c818f9de6e82822
RAFH82 <hrechkar@gmail.com>
2021-03-22 19:53:31 -0700

    refactored code

--- firstNonRepeatingChar.js
+++ firstNonRepeatingChar.js
@@ -1,12 +1,22 @@
-function firstNonRepeatedCharacter(string) {
+// function firstNonRepeatedCharacter(string) {
+// 	for (let i = 0; i < string.length; i++) {
+// 		// iterate through the string
+// 		let letter = string.charAt(i);
+// 		// assign the first letter to a variable
+
+// 		// if the index of that letter  equals the current index, is not found again in the string, starting at the next index in the string (i + 1),
+// 		// return that letter, as indexOf will return -1 if the string does not contain that letter.
+// 		if (string.indexOf(letter) == i && string.indexOf(letter, i + 1) == -1) {
+// 			return letter;
+// 		}
+// 	}
+// 	return null;
+// }
+
+function firstNonRepeatedCharacters(string) {
 	for (let i = 0; i < string.length; i++) {
-		// iterate through the string
-		let letter = string.charAt(i);
-		// assign the first letter to a variable
-		// if the index of that letter at its current index is not found again in the string, starting at the next index in the string,
-		// return that letter, as indexOf will return -1 if the string does not contain that letter.
-		if (string.indexOf(letter) == i && string.indexOf(letter, i + 1) == -1) {
-			return letter;
+		if (string.lastIndexOf(string[i]) == string.indexOf(string[i])) {
+			return string[i];
 		}
 	}
 	return null;
@@ -14,4 +24,5 @@ function firstNonRepeatedCharacter(string) {
 
 let someString = "aabcbd";
 
-console.log(firstNonRepeatedCharacter(someString));
+// console.log(firstNonRepeatedCharacter(someString));
+console.log(firstNonRepeatedCharacters(someString));
