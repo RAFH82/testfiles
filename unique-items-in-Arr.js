614df290050b078f912c66b93f15d4418321ef3a
RAFH82 <hrechkar@gmail.com>
2021-04-20 08:54:50 -0700

    fixed error in console log

--- unique-items-in-Arr.js
+++ unique-items-in-Arr.js
@@ -1,24 +1,28 @@
-function uniqueElements(array) {
-	let uniques = [];
+// function uniqueElements(array) {
+// 	let uniques = [];
 
-	for (let i = 0; i < array.length; i++) {
-		let unique = true;
-		for (let j = 0; j < array.length; j++) {
-			if (i === j) {
-				continue;
-			}
-			if (array[i] === array[j]) {
-				unique = false;
-			}
-		}
-		if (unique) {
-			uniques.push(array[i]);
-		}
-	}
+// 	for (let i = 0; i < array.length; i++) {
+// 		let unique = true;
+// 		for (let j = 0; j < array.length; j++) {
+// 			if (i === j) {
+// 				continue;
+// 			}
+// 			if (array[i] === array[j]) {
+// 				unique = false;
+// 			}
+// 		}
+// 		if (unique) {
+// 			uniques.push(array[i]);
+// 		}
+// 	}
 
-	return uniques;
-}
+// 	return uniques;
+// }
 
 const test = [1, "w", 2, 7, 2, "w", 8, 1, "e", 44, 1];
 
-console.log(uniqueElements(test));
+// console.log(uniqueElements(test));
+
+const uniques = new Set(test);
+
+console.log(uniques);
